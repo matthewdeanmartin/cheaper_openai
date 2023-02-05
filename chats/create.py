c853ead8a21bbc91fe6dd733e9c66ebc206ea9ad
Matthew Martin <matthewdeanmartin@gmail.com>
2023-02-05 16:50:37 -0500

    book writing

--- chats/create.py
+++ chats/create.py
@@ -1,11 +1,11 @@
 import openai
-import tiktoken
 
 from chats.client_utils import create_client
 from chats.io_utils import dump_response, read_config, read_prompt
 from chats.markdown_utils import cleanup_markdown
 from chats.name_this_cheap import create_name
 from chats.spelling_utils import check_document
+from chats.token_utils import count_tokens
 
 config = read_config()
 create_client()
@@ -19,13 +19,7 @@ def run() -> None:
 
     prompt = read_prompt(output_folder)
 
-    # gpt2 (or r50k_base) 	Most GPT-3 models
-    # p50k_base 	Code models, text-davinci-002, text-davinci-003
-    # cl100k_base 	text-embedding-ada-002
-    enc = tiktoken.get_encoding("p50k_base")
-    tokens = enc.encode(prompt)
-    print(len(tokens))
-    prompt_tokens = len(tokens)
+    prompt_tokens = count_tokens(prompt)
 
     prompt = cleanup_markdown(prompt)
     all_right, assistance = check_document(prompt)
--- chats/create_book.py
+++ chats/create_book.py
@@ -5,6 +5,7 @@ import yaml
 
 from chats.client_utils import create_client
 from chats.io_utils import dump_response, read_config, read_prompt, read_yaml_toc_prompt
+from chats.token_utils import count_tokens
 
 config = read_config()
 create_client()
@@ -47,6 +48,10 @@ def run_the_toc():
     temperature = 0.9
     max_tokens = 4000
 
+    who_are_you = (
+        "You are a Linux enthusiast showing Debian users how to use powershell on Linux."
+        " You have a lot of ideas for how to run powershell on linux. You are writing a book. "
+    )
     # get output folder from config file
     output_folder = config["output"]["output_folder"]
     toc = read_yaml_toc_prompt(output_folder)
@@ -58,28 +63,30 @@ def run_the_toc():
             chapter_count = 0
             for chapter in chapters:
                 chapter_count += 1
-                prompt = f"Please write the exposition for '{section}:{chapter}'"
 
+                prompt = f"{who_are_you} Please write the exposition for '{section} : {chapter}'. Use markdown."
                 response = basic_request(max_tokens, prompt, temperature, sleep=5)
-
                 choices = list(x["text"] for x in response["choices"])
                 dump_response(prompt, choices, f"{section}_{chapter_count}", output_folder)
 
-                prompt = f"Please write code samples for '{section}:{chapter}'"
+                prompt = (
+                    f"{who_are_you} Please write code samples for '{section} : {chapter}'. "
+                    f"Use markdown and code blocks for code."
+                )
                 response = basic_request(max_tokens, prompt, temperature, sleep=5)
-
                 choices = list(x["text"] for x in response["choices"])
-                dump_response(prompt, choices, f"{section}_{chapter_count}", output_folder)
+                dump_response(f"{section} : {chapter}", choices, f"{section}_{chapter_count}", output_folder)
 
 
 def basic_request(max_tokens, prompt, temperature, sleep=0):
+    prompt_tokens = count_tokens(prompt)
     print(prompt)
     time.sleep(sleep)
     args = {
         "model": "text-davinci-003",
         "prompt": prompt,
         "temperature": temperature,
-        "max_tokens": max_tokens,
+        "max_tokens": max_tokens - prompt_tokens,
         "top_p": 1,
         "frequency_penalty": 0,
         "presence_penalty": 0,
